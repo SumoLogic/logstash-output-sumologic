c598b6c23c119322d08315e7d36c68da66d172d1
Bin Yi <byi@sumologic.com>
2018-03-13 22:38:21 +0000

    fix unit test and dependency

--- logstash-output-sumologic.gemspec
+++ logstash-output-sumologic.gemspec
@@ -20,5 +20,5 @@ Gem::Specification.new do |s|
   # Gem dependencies
   s.add_runtime_dependency "logstash-core-plugin-api", "~> 2.0"
   s.add_runtime_dependency "logstash-codec-plain", ">= 0"
-  s.add_development_dependency "logstash-devutils", "~> 0"
+  s.add_development_dependency "logstash-devutils", "~> 1.3", ">= 1.3.1"
 end
--- spec/outputs/sumologic_spec.rb
+++ spec/outputs/sumologic_spec.rb
@@ -65,7 +65,9 @@ describe LogStash::Outputs::SumoLogic do
 
   context "log sent with customized json_mapping" do
 
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "format" => "%{host} %{@json}", "json_mapping" => { "foo" => "%{foo}", "bar" => "%{bar}", "%{foo}" => "%{bar}" } ) }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "format" => "%{host} %{@json}", 
+					       "json_mapping" => { "foo" => "%{foo}", "bar" => "%{bar}", "%{foo}" => "%{bar}" } ) }
     let(:event) { LogStash::Event.new("host" => "myHost", "foo" => "fancy", "bar" => 24) }
 
     it "generate one element" do
@@ -96,7 +98,10 @@ describe LogStash::Outputs::SumoLogic do
     let(:event) { LogStash::Event.new("host" => "myHost", "message" => "Hello world") }
 
     it "check header" do
-      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, "X-Sumo-Client"=>"logstash-output-sumologic", "Content-Type"=>"text/plain", "X-Sumo-Category"=>"my source category"})
+      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, 
+				   "X-Sumo-Client"=>"logstash-output-sumologic", 
+				   "Content-Type"=>"text/plain", 
+				   "X-Sumo-Category"=>"my source category"})
     end
 
   end
@@ -107,7 +112,10 @@ describe LogStash::Outputs::SumoLogic do
     let(:event) { LogStash::Event.new("host" => "myHost", "message" => "Hello world") }
 
     it "check header" do
-      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, "X-Sumo-Client"=>"logstash-output-sumologic", "Content-Type"=>"text/plain", "X-Sumo-Name"=>"my source name"})
+      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, 
+				   "X-Sumo-Client"=>"logstash-output-sumologic", 
+				   "Content-Type"=>"text/plain", 
+				   "X-Sumo-Name"=>"my source name"})
     end
 
   end
@@ -140,7 +148,10 @@ describe LogStash::Outputs::SumoLogic do
     let(:event) { LogStash::Event.new("host" => "myHost", "message" => "Hello world") }
 
     it "check header" do
-      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, "X-Sumo-Client"=>"logstash-output-sumologic", "Content-Type"=>"text/plain", "Content-Encoding"=>"deflate"})
+      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, 
+				   "X-Sumo-Client"=>"logstash-output-sumologic", 
+				   "Content-Type"=>"text/plain", 
+				   "Content-Encoding"=>"deflate"})
     end
 
   end
@@ -151,7 +162,10 @@ describe LogStash::Outputs::SumoLogic do
     let(:event) { LogStash::Event.new("host" => "myHost", "message" => "Hello world") }
 
     it "check header" do
-      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, "X-Sumo-Client"=>"logstash-output-sumologic", "Content-Type"=>"text/plain", "Content-Encoding"=>"gzip"})
+      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, 
+				   "X-Sumo-Client"=>"logstash-output-sumologic", 
+				   "Content-Type"=>"text/plain", 
+				   "Content-Encoding"=>"gzip"})
     end
 
   end
@@ -159,7 +173,9 @@ describe LogStash::Outputs::SumoLogic do
   # For metrics
   context "with metrics sent in graphite" do
 
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "metrics" => { "hurray.%{foo}" => "%{bar}"}, "metrics_format" => "graphite") }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "metrics" => { "hurray.%{foo}" => "%{bar}"}, 
+					       "metrics_format" => "graphite") }
     let(:event) { LogStash::Event.new("host" => "myHost", "foo" => "fancy", "bar" => 24) }
 
     it "generate one element" do
@@ -167,7 +183,9 @@ describe LogStash::Outputs::SumoLogic do
     end
 
     it "check header" do
-      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, "X-Sumo-Client"=>"logstash-output-sumologic", "Content-Type"=>"application/vnd.sumologic.graphite"})
+      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, 
+				   "X-Sumo-Client"=>"logstash-output-sumologic", 
+				   "Content-Type"=>"application/vnd.sumologic.graphite"})
     end
 
     it "include all content" do
@@ -185,7 +203,9 @@ describe LogStash::Outputs::SumoLogic do
     end
 
     it "check header" do
-      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, "X-Sumo-Client"=>"logstash-output-sumologic", "Content-Type"=>"application/vnd.sumologic.carbon2"})
+      expect(server.header).to eq({"X-Sumo-Host"=>`hostname`.strip, 
+				   "X-Sumo-Client"=>"logstash-output-sumologic", 
+				   "Content-Type"=>"application/vnd.sumologic.carbon2"})
     end
 
     it "include all content" do
@@ -194,7 +214,9 @@ describe LogStash::Outputs::SumoLogic do
   end
 
   context "with metrics_name override" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "metrics" => { "hurray.%{foo}" => "%{bar}" }, "metrics_name" => "mynamespace.*") }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "metrics" => { "hurray.%{foo}" => "%{bar}" }, 
+					       "metrics_name" => "mynamespace.*") }
     let(:event) { LogStash::Event.new("host" => "myHost", "foo" => "fancy", "bar" => 24) }
 
     it "include all content" do
@@ -203,7 +225,9 @@ describe LogStash::Outputs::SumoLogic do
   end
 
   context "with intrinsic_tags override" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "metrics" => { "bar" => "%{bar}" }, "intrinsic_tags" => {"host"=>"%{host}"}) }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "metrics" => { "bar" => "%{bar}" }, 
+					       "intrinsic_tags" => {"host"=>"%{host}"}) }
     let(:event) { LogStash::Event.new("host" => "myHost", "foo" => "fancy", "bar" => 24) }
 
     it "include all content" do
@@ -212,7 +236,10 @@ describe LogStash::Outputs::SumoLogic do
   end
 
   context "with meta_tags override" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "metrics" => { "bar" => "%{bar}" }, "intrinsic_tags" => {"host"=>"%{host}"}, "meta_tags" => {"foo" => "%{foo}"}) }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "metrics" => { "bar" => "%{bar}" }, 
+					       "intrinsic_tags" => {"host"=>"%{host}"}, 
+					       "meta_tags" => {"foo" => "%{foo}"}) }
     let(:event) { LogStash::Event.new("host" => "myHost", "foo" => "fancy", "bar" => 24) }
 
     it "include all content" do
@@ -221,39 +248,52 @@ describe LogStash::Outputs::SumoLogic do
   end
 
   context "with multiple metrics mapping" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "metrics" => { "cpu1" => "%{cpu1}", "cpu2" => "%{cpu2}" }, "intrinsic_tags" => {"host"=>"%{host}"}, "meta_tags" => {"foo" => "%{foo}"}) }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "metrics" => { "cpu1" => "%{cpu1}", "cpu2" => "%{cpu2}" }, 
+					       "intrinsic_tags" => {"host"=>"%{host}"}, 
+					       "meta_tags" => {"foo" => "%{foo}"}) }
     let(:event) { LogStash::Event.new("host" => "myHost", "foo" => "fancy", "cpu1" => 0.24, "cpu2" => 0.11) }
 
     it "include content" do
-      expect(server.pop).to match(/^host=myHost metric=cpu1  foo=fancy 0\.24 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu2  foo=fancy 0\.11 \d{10,}$/)
+      sorted = server.all_sorted
+      expect(sorted.pop).to match(/^host=myHost metric=cpu1  foo=fancy 0\.24 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu2  foo=fancy 0\.11 \d{10,}$/)
     end
   end
 
   context "metrics with non-number value should be dropped (carbon2)" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "metrics" => { "cpu1" => "%{cpu1}", "cpu2" => "%{cpu2}", "cpu3" => "%{cpu3}" }, "intrinsic_tags" => {"host"=>"%{host}"}, "meta_tags" => {"foo" => "%{foo}"}) }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "metrics" => { "cpu1" => "%{cpu1}", "cpu2" => "%{cpu2}", "cpu3" => "%{cpu3}" }, 
+					       "intrinsic_tags" => {"host"=>"%{host}"}, "meta_tags" => {"foo" => "%{foo}"}) }
     let(:event) { LogStash::Event.new("host" => "myHost", "foo" => "fancy", "cpu1" => 0.24, "cpu2" => "abc", "cpu3" => 0.11) }
 
     it "include content" do
-      expect(server.pop).to match(/^host=myHost metric=cpu1  foo=fancy 0\.24 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu3  foo=fancy 0\.11 \d{10,}$/)
-      expect(server.empty?).to eq(true)
+      sorted = server.all_sorted
+      expect(sorted.pop).to match(/^host=myHost metric=cpu1  foo=fancy 0\.24 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu3  foo=fancy 0\.11 \d{10,}$/)
+      expect(sorted.empty?).to eq(true)
     end
   end
 
   context "metrics with non-number value should be dropped (graphite)" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "metrics" => { "cpu1" => "%{cpu1}", "cpu2" => "%{cpu2}", "cpu3" => "%{cpu3}" }, "metrics_format" => "graphite") }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "metrics" => { "cpu1" => "%{cpu1}", "cpu2" => "%{cpu2}", "cpu3" => "%{cpu3}" }, 
+					       "metrics_format" => "graphite") }
     let(:event) { LogStash::Event.new("host" => "myHost", "foo" => "fancy", "cpu1" => 0.24, "cpu2" => "abc", "cpu3" => 0.11) }
 
     it "include content" do
-      expect(server.pop).to match(/^cpu1 0\.24 \d{10,}$/)
-      expect(server.pop).to match(/^cpu3 0\.11 \d{10,}$/)
-      expect(server.empty?).to eq(true)
+      sorted = server.all_sorted
+      expect(sorted.pop).to match(/^cpu1 0\.24 \d{10,}$/)
+      expect(sorted.pop).to match(/^cpu3 0\.11 \d{10,}$/)
+      expect(sorted.empty?).to eq(true)
     end
   end
 
   context "fields_as_metrics (carbon2)" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "fields_as_metrics" => true, "intrinsic_tags" => {"host"=>"%{host}"}, "meta_tags" => {"foo" => "%{foo}"}) }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "fields_as_metrics" => true, 
+					       "intrinsic_tags" => {"host"=>"%{host}"}, 
+					       "meta_tags" => {"foo" => "%{foo}"}) }
     let(:event) { LogStash::Event.new(
       "host" => "myHost", 
       "foo" => "fancy", 
@@ -267,15 +307,16 @@ describe LogStash::Outputs::SumoLogic do
       })}
 
     it "include content" do
-      expect(server.pop).to match(/^host=myHost metric=blkio\.write_ps  foo=fancy 5 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=blkio\.read_ps  foo=fancy 2 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=blkio\.total_ps  foo=fancy 0 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.0  foo=fancy 0\.24 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.1  foo=fancy 0\.11 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.2  foo=fancy 0\.75 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.3  foo=fancy 0\.28 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=storageRW  foo=fancy 51 \d{10,}$/)
-      expect(server.empty?).to eq(true)
+      sorted = server.all_sorted
+      expect(sorted.pop).to match(/^host=myHost metric=blkio\.read_ps  foo=fancy 2 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=blkio\.total_ps  foo=fancy 0 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=blkio\.write_ps  foo=fancy 5 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.0  foo=fancy 0\.24 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.1  foo=fancy 0\.11 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.2  foo=fancy 0\.75 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.3  foo=fancy 0\.28 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=storageRW  foo=fancy 51 \d{10,}$/)
+      expect(sorted.empty?).to eq(true)
     end
   end
 
@@ -294,19 +335,24 @@ describe LogStash::Outputs::SumoLogic do
       })}
 
     it "include content" do
-      expect(server.pop).to match(/^blkio.write_ps 0 \d{10,}$/)
-      expect(server.pop).to match(/^blkio.read_ps 0 \d{10,}$/)
-      expect(server.pop).to match(/^blkio.total_ps 0 \d{10,}$/)
-      expect(server.pop).to match(/^cpu\.0 0\.24 \d{10,}$/)
-      expect(server.pop).to match(/^cpu\.1 0\.11 \d{10,}$/)
-      expect(server.pop).to match(/^cpu\.2 0\.75 \d{10,}$/)
-      expect(server.pop).to match(/^cpu\.3 0\.28 \d{10,}$/)
-      expect(server.pop).to match(/^storageRW 51 \d{10,}$/)
+      sorted = server.all_sorted
+      expect(sorted.pop).to match(/^blkio\.read_ps 0 \d{10,}$/)
+      expect(sorted.pop).to match(/^blkio\.total_ps 0 \d{10,}$/)
+      expect(sorted.pop).to match(/^blkio\.write_ps 0 \d{10,}$/)
+      expect(sorted.pop).to match(/^cpu\.0 0\.24 \d{10,}$/)
+      expect(sorted.pop).to match(/^cpu\.1 0\.11 \d{10,}$/)
+      expect(sorted.pop).to match(/^cpu\.2 0\.75 \d{10,}$/)
+      expect(sorted.pop).to match(/^cpu\.3 0\.28 \d{10,}$/)
+      expect(sorted.pop).to match(/^storageRW 51 \d{10,}$/)
     end
   end
 
   context "fields_include is honored when fields_as_metrics (carbon2)" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "fields_as_metrics" => true, "intrinsic_tags" => {"host"=>"%{host}"}, "meta_tags" => {"foo" => "%{foo}"}, "fields_include" => ["cpu*"]) }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "fields_as_metrics" => true, 
+					       "intrinsic_tags" => {"host"=>"%{host}"}, 
+					       "meta_tags" => {"foo" => "%{foo}"}, 
+					       "fields_include" => ["cpu*"]) }
     let(:event) { LogStash::Event.new(
       "host" => "myHost", 
       "foo" => "fancy", 
@@ -320,16 +366,22 @@ describe LogStash::Outputs::SumoLogic do
       })}
 
     it "include content" do
-      expect(server.pop).to match(/^host=myHost metric=cpu\.0  foo=fancy 0\.24 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.1  foo=fancy 0\.11 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.2  foo=fancy 0\.75 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.3  foo=fancy 0\.28 \d{10,}$/)
-      expect(server.empty?).to eq(true)
+      sorted = server.all_sorted
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.0  foo=fancy 0\.24 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.1  foo=fancy 0\.11 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.2  foo=fancy 0\.75 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.3  foo=fancy 0\.28 \d{10,}$/)
+      expect(sorted.empty?).to eq(true)
     end
   end
 
   context "fields_exclude is honored when fields_as_metrics (carbon2)" do
-    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", "fields_as_metrics" => true, "intrinsic_tags" => {"host"=>"%{host}"}, "meta_tags" => {"foo" => "%{foo}"}, "fields_include" => ["cpu*"], "fields_exclude" => [".*1"]) }
+    subject { LogStash::Outputs::SumoLogic.new("url" => "http://localhost/1234", 
+					       "fields_as_metrics" => true, 
+					       "intrinsic_tags" => {"host"=>"%{host}"}, 
+					       "meta_tags" => {"foo" => "%{foo}"}, 
+					       "fields_include" => ["cpu*"], 
+					       "fields_exclude" => [".*1"]) }
     let(:event) { LogStash::Event.new(
       "host" => "myHost", 
       "foo" => "fancy", 
@@ -343,10 +395,11 @@ describe LogStash::Outputs::SumoLogic do
       })}
 
     it "include content" do
-      expect(server.pop).to match(/^host=myHost metric=cpu\.0  foo=fancy 0\.24 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.2  foo=fancy 0\.75 \d{10,}$/)
-      expect(server.pop).to match(/^host=myHost metric=cpu\.3  foo=fancy 0\.28 \d{10,}$/)
-      expect(server.empty?).to eq(true)
+      sorted = server.all_sorted
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.0  foo=fancy 0\.24 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.2  foo=fancy 0\.75 \d{10,}$/)
+      expect(sorted.pop).to match(/^host=myHost metric=cpu\.3  foo=fancy 0\.28 \d{10,}$/)
+      expect(sorted.empty?).to eq(true)
     end
   end
 
